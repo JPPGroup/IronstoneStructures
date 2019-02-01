c44e51c99232fc54271dd5d8ae36fb79b684a32b
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-02-01 14:08:12 +0000

    Initial repo

---
+++ StructuresExtensionApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jpp.Ironstone.Core;
+using Unity;
+
+namespace Jpp.Ironstone.Structures
+{
+    class StructuresExtensionApplication : IIronstoneExtensionApplication
+    {
+        public void CreateUI()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InjectContainer(IUnityContainer container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
