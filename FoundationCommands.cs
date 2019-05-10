cbcf3cf926f9059303560f12ccf193ec220e9317
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-05-10 14:11:26 +0100

    Added basic foundation command implementation

--- FoundationCommands.cs
+++ FoundationCommands.cs
@@ -7,10 +7,10 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
+using Jpp.Ironstone.Core.Autocad;
 using Jpp.Ironstone.Core.ServiceInterfaces;
-using Jpp.Ironstone.Structures.Objectmodel;
-using Jpp.Ironstone.Structures.Objectmodel.Foundations;
-using Jpp.Ironstone.Structures.Objectmodel.TreeRings;
+using Jpp.Ironstone.Structures.ObjectModel;
+using Jpp.Ironstone.Structures.ObjectModel.Foundations;
 
 namespace Jpp.Ironstone.Structures
 {
@@ -34,6 +34,10 @@ namespace Jpp.Ironstone.Structures
                     FoundationManager manager = DataService.Current.GetStore<StructureDocumentStore>(acDoc.Name)
                         .GetManager<FoundationManager>();
 
+                    SoilProperties sp = DataService.Current.GetStore<StructureDocumentStore>(acDoc.Name).SoilProperties;
+                    BlockTableRecord modelSpace = acDoc.Database.GetModelSpace();
+                    modelSpace.UpgradeOpen();
+
                     // Step through the objects in the selection set
                     foreach (SelectedObject acSSObj in acSSet)
                     {
@@ -42,18 +46,38 @@ namespace Jpp.Ironstone.Structures
                         {
                             // Open the selected object for write
                             Entity acEnt = acTrans.GetObject(acSSObj.ObjectId, OpenMode.ForRead) as Entity;
-                            
                             if (acEnt is Curve)
                             {
-                                FoundationCentreline foundationCentreline = new FoundationCentreline();
-                                foundationCentreline.BaseObject = acEnt.ObjectId;
-                                manager.Add(foundationCentreline);
+                                DBObjectCollection parts = new DBObjectCollection();
+                                acEnt.Explode(parts);
+
+                                if (parts.Count > 1)
+                                {
+                                    acEnt.UpgradeOpen();
+                                    acEnt.Erase();
+                                    foreach (DBObject dbObject in parts)
+                                    {
+                                        FoundationCentreLine foundationCentreline = new FoundationCentreLine(sp);
+                                        foundationCentreline.BaseObject = modelSpace.AppendEntity(dbObject as Entity);
+                                        manager.Add(foundationCentreline);
+
+                                        acTrans.AddNewlyCreatedDBObject(dbObject, true);
+                                    }
+                                }
+                                else
+                                {
+                                    FoundationCentreLine foundationCentreline = new FoundationCentreLine(sp);
+                                    foundationCentreline.BaseObject = acEnt.ObjectId;
+                                    manager.Add(foundationCentreline);
+                                }
                             }
                         }
                     }
 
                     manager.UpdateDirty();
                 }
+
+                acTrans.Commit();
             }
         }
     }
