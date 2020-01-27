a464af5436c74a7864c567c773bdff2e8154f23e
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-01-27 07:36:16 +0000

    Package update

--- StructuresExtensionApplication.cs
+++ StructuresExtensionApplication.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.AutoCAD.Customization;
-using Autodesk.AutoCAD.Runtime;
+﻿using Autodesk.AutoCAD.Runtime;
 using Autodesk.Windows;
 using Jpp.Ironstone.Core;
 using Jpp.Ironstone.Core.ServiceInterfaces;
